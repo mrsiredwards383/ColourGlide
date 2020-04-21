71f3cabe550a8b724f6c1096b4ea9ebdbb4ab82e
[mrsiredwards] <ryanmedwards@gmail.com>
2020-04-21 08:38:02 -0400

    Add Canvas and fill page colour

--- myscripts.js
+++ myscripts.js
@@ -1,4 +1,15 @@
 
-function myFunction() {
-  document.getElementById("demo").innerHTML = "Hello World";
+
+
+function colourCan() {
+
+var c = document.getElementById("colourCanvas");
+var ctx = c.getContext("2d");
+c.setAttribute('width', window.innerWidth);
+c.setAttribute('height', window.innerHeight);
+
+
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(0, 0, c.width, c.height);
+
 };
